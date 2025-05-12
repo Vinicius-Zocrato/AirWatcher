800166167fb4c082d869f83fd0ccabbd8d504fb2
Marcos-Ribas <ribas.sjp@gmail.com>
2025-05-12 17:45:42 +0200

    aaa

--- Include/Domain/Attribute.h
+++ Include/Domain/Attribute.h
@@ -17,38 +17,7 @@ class Attribute {
         string attributeID;
         string unit;
         string description;
-class Attribute
-{
-public:
-    Attribute(string attributeID, string unit, string description);
-    Attribute();
-    ~Attribute();
-    void setAttributeID(const string &attributeID) { this->attributeID = attributeID; }
-    void setUnit(const string &unit) { this->unit = unit; }
-    void setDescription(const string &description) { this->description = description; }
-
-private:
-    string attributeID;
-    string unit;
-    string description;
 };
 
-istream &operator>>(istream &is, Attribute &attr)
-{
-    string line;
-    if (getline(is, line))
-    {
-        stringstream ss(line);
-        string attributeID, unit, description;
-        getline(ss, attributeID, ';');
-        attr.setAttributeID(attributeID);
-        ss.ignore(1);
-        getline(ss, unit, ';');
-        attr.setUnit(unit);
-        ss.ignore(1);
-        getline(ss, description, ';');
-        attr.setDescription(description);
-    }
-    return is;
-}
+
 #endif // Attribute_H
