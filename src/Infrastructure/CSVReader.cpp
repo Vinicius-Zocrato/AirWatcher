2615cd694db30a72bd27bc92c461daab9de5f5dd
Marcos-Ribas <ribas.sjp@gmail.com>
2025-05-12 16:19:22 +0200

    CSVReader::loadAttributes()

--- src/Infrastructure/CSVReader.cpp
+++ src/Infrastructure/CSVReader.cpp
@@ -14,33 +14,75 @@ CSVReader::~CSVReader() {
 }
 
 
-std::vector<Sensor> CSVReader::loadSensors() const {
+std::vector<Sensor> CSVReader::loadSensors()  {
     
     return {};
 }
 
-std::vector<Measurement> CSVReader::loadMeasurements() const {
+std::vector<Measurement> CSVReader::loadMeasurements()  {
     
     return {};
 }
 
-std::vector<User> CSVReader::loadUsers() const {
+std::vector<User> CSVReader::loadUsers()  {
     
     return {};
 }
 
-std::vector<Provider> CSVReader::loadProviders() const {
+std::vector<Provider> CSVReader::loadProviders()  {
     
     return {};
 }
 
-std::vector<Cleaner> CSVReader::loadCleaners() const {
+std::vector<Cleaner> CSVReader::loadCleaners()  {
     
     return {};
 }
 
-std::vector<Attribute> CSVReader::loadAtributes() const{
+std::vector<Attribute> CSVReader::loadAttributes() {
 
+    std::ifstream file(this->filename);
+    if (!file) {
+        std::cerr << "Error, open File\n";
+        return {};
+    }
 
-    return {};
+    std::vector<std::string> AttributeID;
+    std::vector<std::string> Unit;
+    std::vector<std::string> Description;
+
+    std::string linha;
+    while (std::getline(file, linha)) {
+        if (linha.empty()) continue;  
+
+        std::stringstream ss(linha);
+        std::string s1, s2, s3;
+
+        
+        if (!std::getline(ss, s1, ';')) continue;
+        
+        if (!std::getline(ss, s2, ';')) continue;
+        
+        if (!std::getline(ss, s3, ';')) continue;
+
+        
+        try {
+            // armazena
+            AttributeID.push_back(s1);
+            Unit.push_back(s2);
+            Description.push_back(s3);
+        } catch (const std::exception& e) {
+            std::cerr << "Erro convert values: " << linha << "\n";
+        }
+    }
+
+    std::vector<Attribute> attributes;
+
+    for(int i=0; i<AttributeID.size(); i++){
+        attributes.push_back(Attribute(AttributeID[i], Unit[i], Description[i]));
+    }
+
+    this->attributes = attributes;
+
+    return attributes;
 }
