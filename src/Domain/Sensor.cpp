7d9e867ced43c66c47f00d14acba8526c5030c96
Imane <imane@DESKTOP-FNCDDI6>
2025-05-12 15:52:04 +0200

    changement time time en tm dans cleanerh

--- src/Domain/Sensor.cpp
+++ src/Domain/Sensor.cpp
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-#include "../../Include/Domain/Sensor.h"
-
-
-Sensor::Sensor() {
-
-}
-
-Sensor::Sensor(string sensorID, float lat, float longitude, bool status) {
-    this->sensorID = sensorID;
-    this->latitude = lat;
-    this->longitude = longitude;
-    this->status = status;
-}
-
-
-Sensor::~Sensor() {
-
-}
-=======
 #include "../../Include/Domain/Sensor.h"
 
 
@@ -62,4 +42,3 @@ float Sensor::getLongitude(){
 float Sensor::getStatus(){
     return this->status;
 }
->>>>>>> 581e268f6645e444e8210b7d9e69c6aba0d775a2
