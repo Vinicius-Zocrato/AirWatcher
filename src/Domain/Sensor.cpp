9bec869abfb00e552ecebbc37f34bb5d333610f9
Marcos-Ribas <ribas.sjp@gmail.com>
2025-05-12 16:07:00 +0200

    Update

--- src/Domain/Sensor.cpp
+++ src/Domain/Sensor.cpp
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
 #include "../../Include/Domain/Sensor.h"
-
-
-Sensor::Sensor() {
-
-}
-
-Sensor::Sensor(string sensorID, float lat, float longitude, bool status) {
-    this->sensorID = sensorID;
-    this->latitude = lat;
-    this->longitude = longitude;
-    this->status = status;
-}
-
-
-Sensor::~Sensor() {
-
-}
-=======
-#include "../../Include/Domain/Sensor.h"
-
-
 Sensor::Sensor() {
 
 }
@@ -62,4 +40,3 @@ float Sensor::getLongitude(){
 float Sensor::getStatus(){
     return this->status;
 }
->>>>>>> 581e268f6645e444e8210b7d9e69c6aba0d775a2
