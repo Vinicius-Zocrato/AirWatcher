b64c062bd3a8ae68dea2a8b6cd85d96a9810eeb1
Vinicius Zocrato <vi.zocrato@gmail.com>
2025-05-12 14:45:05 +0200

    Creation of main and ConsoleUI.cpp

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include "Include/Domain/Attribute.h"
+#include "Include/Domain/Measurement.h"
+#include "Include/Domain/Sensor.h"
+#include "Include/Presentation/ConsoleUI.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    displayMenu();
+
+}
