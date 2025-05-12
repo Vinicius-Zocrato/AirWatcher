e391c50a154ff15b6a6c561307667fd985359c75
xamenss <maxenceheurtault04@gmail.com>
2025-05-12 17:13:45 +0200

    AirqualityAnalyzer::findSensorbyId

--- Include/Domain/AirQualityAnalyzer.h
+++ Include/Domain/AirQualityAnalyzer.h
@@ -18,8 +18,7 @@ public:
     double calculateAirQuality();
     double calculateSimilarity(const std::vector<Measurement>& m1,
     const std::vector<Measurement>& m2);
-    vector<Sensor> findMostSimilarSensors(Sensor target,
-vector<Sensor> others, int topN);
+    vector<Sensor> findMostSimilarSensors(Sensor target,vector<Sensor> others, int topN);
     double computeAverage();
     vector<Sensor> rankSensorByQuality();
     Sensor findSensorById(const string capteurId ) const;
