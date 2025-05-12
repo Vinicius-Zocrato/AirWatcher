5ef3ee8358e4026423ae46a6a56f154e04ed540f
Imane <imane@DESKTOP-FNCDDI6.>
2025-05-12 14:48:19 +0200

    user.cpp

---
+++ src/Domain/User.cpp
@@ -0,0 +1,27 @@
+#include "../../Include/Domain/User.h"
+#include "../../Include/Domain/Sensor.h"
+
+
+User::User()
+    : userId(""), score(0), isReliable(true) {
+    
+}
+
+// Destructeur
+User::~User() {
+}
+
+// Renvoie la liste des capteurs associés
+vector<Sensor> User::getAssociatedSensors() const {
+    return associatedSensors;
+}
+
+
+void User::updateScore() {
+    // Exemple : augmenter le score de 1
+    score += 1;
+}
+
+void User::changeRealisable() {
+    isReliable = !isReliable;
+}
