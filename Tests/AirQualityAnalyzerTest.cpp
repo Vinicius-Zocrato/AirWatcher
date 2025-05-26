817fa0092402441253602c541001b9f57a8bcc67
xamenss <maxenceheurtault04@gmail.com>
2025-05-26 17:11:47 +0200

    fichier de Test

--- Tests/AirQualityAnalyzerTest.cpp
+++ Tests/AirQualityAnalyzerTest.cpp
@@ -1,6 +1,6 @@
-// #include <cassert>
-// #include "AirQualityAnalyzer.h"
-// #include "Sensor.h"
+#include <cassert>
+#include "AirQualityAnalyzer.h"
+#include "Sensor.h"
 
 // int main() {
 //     AirQualityAnalyzer analyzer;
@@ -18,3 +18,33 @@
 
 //     return 0;
 // }
+
+#include "SensorValidator.h"
+#include "Sensor.h"
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
+
+int main(int argc, char* argv[])
+{
+
+    AirQualityAnalyzer analyzer;
+
+    if (argc > 2)
+    {
+        cerr << ("Arguments trop nombreux ou Invalide ") << endl;
+        return -1;
+    }
+    int caseTest = atoi(argv[argc]);
+    if (caseTest == 1)
+    {
+        //Test1
+        
+    }
+   
+    AirQualityAnalyzer analyzer;
+    std::vector<Measurement> emptyMeasurements;
+    double avg = analyzer.computeAvarege("O3", emptyMeasurements);
+    std::cout << "Test computeAverage - Empty list: " << (avg == 0.0 ? "PASS" : "FAIL") << "\n";
+}
--- Tests/SensorValidatorTest.cpp
+++ Tests/SensorValidatorTest.cpp
@@ -1,8 +1,29 @@
 #include "SensorValidator.h"
+#include "Sensor.h"
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
 
 int main(int argc, char* argv[])
 {
 
-    printf("test nbarg: %d", argc);
+    if (argc > 2)
+    {
+        cerr << ("Arguments trop nombreux ou Invalide ") << endl;
+        return -1;
+    }
+    int caseTest = atoi(argv[argc]);
+    if (caseTest == 1)
+    {
+        //Test1
+        Sensor s("S1", -999.0, 999.0, "User1"); // clearly invalid coordinates
+        SensorValidator validator;
+        bool result = validator.isValidSensor(s);
+        std::cout << "Test isValidSensor - Invalid coordinates: " << (result == false ? "PASS" : "FAIL") << "\n";
+        
+        return result;
+    }
+   
 
 }
